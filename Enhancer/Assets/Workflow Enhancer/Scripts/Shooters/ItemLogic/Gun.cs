2e7edace0e2535000de12d8039f6e3605eff42c4
Kaylerr <71836631+Kaylerr@users.noreply.github.com>
2023-10-01 20:02:45 +0100

    Workflow Enhancer 0.10.2
    - Audio FX for guns
    - general QoL improvements, but only tiny ones
    
    Since this is getting a little love on YouTube, we're gonna make tremendous quality improvements.

--- Enhancer/Assets/Workflow Enhancer/Scripts/Shooters/ItemLogic/Gun.cs
+++ Enhancer/Assets/Workflow Enhancer/Scripts/Shooters/ItemLogic/Gun.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Gun : AbstractGun
 {
 	[SerializeField] Camera cam;
-
+	[SerializeField] AudioClip sound;
+	[SerializeField] AudioSource SFXPlayer;
 
 	public override void Use()
 	{
@@ -20,14 +21,19 @@ public class Gun : AbstractGun
 		{
 			hit.collider.gameObject.GetComponent<IDamageable>()?.TakeDamage(((GunCreator)itemInfo).Damage);
 			Collider[] colliders = Physics.OverlapSphere(hit.point, 0.3f);
-			
+
 			if (colliders.Length != 0)
 			{
-				if(colliders[0].tag == "Impact")
+				if (colliders[0].tag == "Impact")
 					return;
-					
+
 				GameObject bulletImpactObj = Instantiate(bulletImpactPrefab, hit.point + hit.normal * 0.001f, Quaternion.LookRotation(hit.normal, Vector3.up) * bulletImpactPrefab.transform.rotation);
 				bulletImpactObj.name = "Impact VFX";
+				
+				if (SFXPlayer != null)
+					if (sound != null)
+						SFXPlayer.PlayOneShot(sound);
+
 				Destroy(bulletImpactObj, 2f);
 				bulletImpactObj.transform.SetParent(colliders[0].transform);
 			}
